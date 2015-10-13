6082206cf74128a43dae511dffd32eebc0171379
SethShi <2108299930@qq.com>
2015-10-13 16:25:53 +0800

    test

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -16,6 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    int x = 0;
+    int y = 1;
+    NSLog(@"%d,%d",x,y);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
